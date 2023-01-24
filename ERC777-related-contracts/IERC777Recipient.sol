ff9d103302590048aa7832d3d2248748b85b1aa3
ryojiroakiyama <ryojiro25@gmail.com>
2023-01-24 10:11:30 +0900

    done IERC777Recipient

--- ERC777-related-contracts/IERC777Recipient.sol
+++ ERC777-related-contracts/IERC777Recipient.sol
@@ -3,6 +3,20 @@
 
 pragma solidity ^0.8.0;
 
+// `IERC777Recipient`は`tokensReceived`という関数の実装を定義するインタフェースです.
+// `tokensReceived`はあるアカウントに対して「トークンが転送される」=「トークンが増加する」アクションが起きた際に, 
+// そのことをアカウント(トークン受信者)に通知する役目を担います.
+//
+// 以下に`IERC777Recipient`と`tokensReceived`が使用される場面を示します. 先に`ERC777`と`IERC1820`のファイル冒頭の説明に目を通してください.
+// 登場人物: `ERC777`のholderであるH. HのoperatorであるO.
+// 前提: Hは, 自身にトークンが転送される際に
+//      通知または何らかのアクション(特定の条件下では転送のトランザクションをキャンセルさせるなど)を起こしたいと考えているとします.
+//      Hは, その処理を実装したコントラクト(つまり`IERC777Recipient`を実装し`tokensReceived`関数に処理を記述したコントラクト)を用意し, `IERC1820`に登録しています.
+// 1. OがHにトークンを送信(または発行)しようと`ERC777`のsend関数を呼び出します.
+// 2. send関数内では, Hの残高を変更後にHが`IERC777Recipient`を実装したコントラクトを登録しているか`IERC1820`に問い合わせます.
+// 3. 2がYesの場合, 登録されているコントラクトの`tokensReceived`関数を呼び出します. Noの場合の処理は別であります.
+// 4. 3の終了後, トランザクションのキャンセル等起きなければトークンの転送処理が完了します.
+
 /**
  * @dev Interface of the ERC777TokensRecipient standard as defined in the EIP.
  *
@@ -14,6 +28,16 @@ pragma solidity ^0.8.0;
  * See {IERC1820Registry} and {ERC1820Implementer}.
  */
 interface IERC777Recipient {
+    // この関数はholderの残高が変更された後に呼び出される必要がある.
+    // revert処理を入れることでトークンの転送をキャンセルすることが可能.
+    // msg.senderはERC777であることが期待される.
+    // 引数の内容は以下です.
+    // operator: operator
+    // from: holder(0x0の場合はトークンが発行されたことを指す)
+    // to: トークンの転送先
+    // amount: 転送量
+    // userData: (空でなければ)転送に付与されたデータ
+    // operatorData: (空でなければ)operatorによって付与されたデータ
     /**
      * @dev Called by an {IERC777} token contract whenever tokens are being
      * moved or created into a registered account (`to`). The type of operation
